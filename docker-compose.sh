5d8e4562f4b76474e43467c72797cd50f23c6bae
Mantena Durga Prasad Raju <78532742+dprajum@users.noreply.github.com>
2025-10-24 16:15:10 +0530

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,5 +1,6 @@
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-ls /usr/local/bin/
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo yum install -y python-pip
+sudo pip3 install docker-compose
 docker-compose version
